0392924e558a33dd62e9ff6acafcad84077a6e21
ksscott <scott.ksteven@gmail.com>
2022-03-29 21:11:15 -0600

    Unit hashcode and equals should remove orders on death

--- src/models/Unit.java
+++ src/models/Unit.java
@@ -47,4 +47,34 @@ public class Unit implements Drawable, Comparable<Unit> {
 			return player.playerIndex - o.player.playerIndex;
 		}
 	}
+
+	// I tinkered in here. I feel confident I got it right. Famous last words...
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + index;
+		result = prime * result + ((player == null) ? 0 : player.playerIndex);
+		return result;
+	}
+
+	// I tinkered in here. I feel confident I got it right. Famous last words...
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Unit other = (Unit) obj;
+		if (index != other.index)
+			return false;
+		if (player == null) {
+			if (other.player != null)
+				return false;
+		} else if (player.playerIndex != other.player.playerIndex)
+			return false;
+		return true;
+	}
 }
